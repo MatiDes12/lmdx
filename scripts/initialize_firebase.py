8862e7b283cfea434b82828bc7afa666b9550512
Roberadesissai <roberadesissakit@gmail.com>
2024-07-19 15:24:52 -0400

    update

--- scripts/initialize_firebase.py
+++ scripts/initialize_firebase.py
@@ -1,10 +1,23 @@
-# initialize_firebase.py
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+import os
+import json  # Import json module to parse the JSON string
+
+# Load environment variables
+load_dotenv()
 
 def initialize_firebase():
-    # Path to your service account JSON file
-    cred = credentials.Certificate('./app/config/firebase-adminsdk.json')
+    # Retrieve JSON string from environment variable
+    firebase_json = os.getenv('FIREBASE_ADMINSDK_JSON')
+    if not firebase_json:
+        raise ValueError("Missing or invalid FIREBASE_ADMINSDK_JSON in .env file")
+
+    # Parse the JSON string into a Python dictionary
+    firebase_credentials = json.loads(firebase_json)
+
+    # Use the parsed credentials dictionary to initialize Firebase
+    cred = credentials.Certificate(firebase_credentials)
     
     # Explicitly set the Firebase project ID
     firebase_admin.initialize_app(cred, {
