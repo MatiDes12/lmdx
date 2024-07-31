2d79de187ca0f4627bf142f99eb0bf1762c0f5aa
Roberadesissai <roberadesissakit@gmail.com>
2024-07-31 13:14:56 -0400

    update

--- app/models_db.py
+++ app/models_db.py
@@ -9,7 +9,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)  # Add username field
+    username = db.Column(db.String(50), unique=False, nullable=False)  # Add username field
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     user_type = db.Column(db.Enum('patient', 'doctors', 'staff', 'admin', 'organization', name='user_type_enum'), nullable=False)
