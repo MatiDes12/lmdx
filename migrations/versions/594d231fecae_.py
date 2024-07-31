3c174c467ba3dc42d20e682e164f133b72e4935a
Roberadesissai <roberadesissakit@gmail.com>
2024-07-30 21:18:15 -0400

    update

--- migrations/versions/594d231fecae_.py
+++ migrations/versions/594d231fecae_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8bb4221d6ec8
+Revision ID: 594d231fecae
 Revises: 
-Create Date: 2024-07-30 18:26:06.815397
+Create Date: 2024-07-30 20:27:21.465800
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8bb4221d6ec8'
+revision = '594d231fecae'
 down_revision = None
 branch_labels = None
 depends_on = None
